7d78875c8dcaa87dc9e18cc26a0fbf102fe7c8c8
Mahesh17-m <mbmandalapu200217@gmail.com>
2024-11-24 23:00:46 +0530

    Change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LifecycleDemoComponent } from './lifecyle-demo/lifecyle-demo.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,LifecycleDemoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
